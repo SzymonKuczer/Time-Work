dfe2c35a2836fa153eaa219ec476cf93c49d98a6
Szymon Kuczer <74829071+SzymonKuczer@users.noreply.github.com>
2023-09-13 11:38:17 +0100

    Add files via upload

---
+++ Time 1 Circle properties/Time 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_1
+{
+    class Program
+    {
+        static double Discount(double Total, double Rate)
+        {
+            return Total - (Total * Rate / 100);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Discount(55,20));
+        }
+    }
+}
