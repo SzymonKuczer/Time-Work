72ff1d7882a98aadc6de5f0b280de4db589e8468
Szymon Kuczer <74829071+SzymonKuczer@users.noreply.github.com>
2023-09-13 11:49:19 +0100

    Add files via upload

---
+++ Energy Bill Calc/Energy Bill Calc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_1
+{
+    class Program
+    {
+        static double EnergyCalculator(double PrevReading, double CurrentReading, double CalorificValue)
+        {
+            double UnitsUsed = CurrentReading - PrevReading;
+            double KWh = UnitsUsed * 1.022 * (CalorificValue / 3.6);
+            double Price = 2.84 * KWh;
+            return Price;
+        }
+
+        static void Main(string[] args)
+        {
+            double PrevReading = 200;
+            double CurrentReading = 400;
+            const double CALORIFIC_VALUE = 39.3;
+
+            Console.WriteLine(EnergyCalculator(PrevReading, CurrentReading, CALORIFIC_VALUE));
+
+        }
+    }
+}
